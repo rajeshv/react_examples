0a7c67a6c10ae441ea9b52bdb3fed7073313e674
Rajesh Veedhi <rajesh.venkat.veedhi@gmail.com>
2017-04-03 12:29:15 +0530

    [CHANGE] Simple sample Routing App

--- routing-example/src/App.js
+++ routing-example/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Router, Route, hashHistory } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
       <div className="App">
@@ -18,4 +19,30 @@ class App extends Component {
   }
 }
 
-export default App;
+
+class Home extends Component {
+  render(){
+    return (<h1>Home Page</h1>);
+  }
+}
+
+// More components
+class Contact extends Component {
+  render(){
+    return (<h1>Contact page</h1>);
+  }
+}
+
+class About extends Component {
+  render(){
+    return (<h1>About page</h1>);
+  }
+}
+
+export default (
+  <Router history={hashHistory}>
+    <Route path="/" component={Home}/>
+    <Route path="/contact" component={Contact}/>
+    <Route path="/about" component={About}/>
+  </Router>
+);
